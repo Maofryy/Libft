23d79519a0d1d9f137da24dcef0aa6d0d5ff2b66
Maori BENHASSINE <mbenhass@e2r12p19.42.fr>
2018-11-07 16:05:45 +0100

    Adding memcmp tested and normed

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mbenhass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 10:05:59 by mbenhass          #+#    #+#             */
-/*   Updated: 2018/11/07 15:51:44 by mbenhass         ###   ########.fr       */
+/*   Updated: 2018/11/07 15:59:52 by mbenhass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
 #endif
